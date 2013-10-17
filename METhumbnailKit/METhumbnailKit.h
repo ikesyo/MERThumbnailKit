4cca530baf5a1ea6d62befd93d2aea79ed0c1dd6
William Towe <willbur1984@gmail.com>
2013-10-16 20:46:36 -0400

    Initial Commit

---
+++ METhumbnailKit/METhumbnailKit.h
@@ -0,0 +1,13 @@
+//
+//  METhumbnailKit.h
+//  METhumbnailKit
+//
+//  Created by William Towe on 10/16/13.
+//  Copyright (c) 2013 Maestro, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface METhumbnailKit : NSObject
+
+@end
---
+++ METhumbnailKit/METhumbnailKit.m
@@ -0,0 +1,13 @@
+//
+//  METhumbnailKit.m
+//  METhumbnailKit
+//
+//  Created by William Towe on 10/16/13.
+//  Copyright (c) 2013 Maestro, LLC. All rights reserved.
+//
+
+#import "METhumbnailKit.h"
+
+@implementation METhumbnailKit
+
+@end
